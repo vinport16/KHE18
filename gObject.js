50bd5edfc5284baf35f772162dd1f9a9ee37c9ff
Michael Smith <mes258@case.edu>
2018-10-19 21:37:54 -0400

    made some objects

--- gObject.js
+++ gObject.js
@@ -1,3 +1,12 @@
 //gameObjects
 //projectiles 
 //explosions
+
+function GameObject(pos){
+	this.position = pos;
+}
+
+GameObject.prototype.delete = function(state) {
+    var index = state.world.indexOf(this);
+	if (index !== -1) state.world.splice(index, 1);
+};
