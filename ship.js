e56664bf1edc8877eeead1d2435308576b27c70e
Michael Smith <mes258@case.edu>
2018-10-20 00:25:40 -0400

    Added ships

--- ship.js
+++ ship.js
@@ -1,1 +1,14 @@
 //ship.js
+
+function Ship(pos, state) {
+	this.radius = 20;
+	GameObject.call(this, pos);
+
+	this.health = 100;
+	this.color = "red";
+	this.bufferTime = 10;
+	this.projectileSpeed = 10; 
+	this.projectileDamage = 30;
+}
+Ship.prototype = Object.create(GameObject.prototype);
+Ship.prototype.constructor = Ship;
