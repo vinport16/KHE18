4de6f83e8db9c1fe9e28876ad6a6b546e042188e
Michael Smith <mes258@case.edu>
2018-10-19 21:18:01 -0400

    Added gameState

--- gameState.js
+++ gameState.js
@@ -1,1 +1,14 @@
 //gameState.js
+
+function GameState() {
+	this.world = []; //list of every game object
+	this.position = {x:0,y:0};
+	this.money = 1000;
+	this.level = 0;
+	this.shipsKilled = 0;
+	this.currentStep = 0;
+}; 
+
+// GameState.prototype.step = function() {
+//     this.step++;
+// };
