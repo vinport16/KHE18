ce5f24a982bad738c24ee2491e1ea10bfe4881f3
Vincent Portelli <vinport16@gmail.com>
2018-10-20 16:57:25 -0400

    removed stuff from main

--- main.js
+++ main.js
@@ -3,46 +3,16 @@ function sleep(ms) {
 }
 
 state = new GameState();
+state.money = 1000000;
 state.position = {x:-canvas.width/2,y:-canvas.height/2};
-paused = true;
-pause();
+
+paused = false;
+pause(); //immediately pause the game
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function makeTowers(n,t){
-  i = 0;
-  while(i<n/t){
-    a = new Tower({x:i*70+20,y:i+75},state);
-    state.world.push(a);
-    i++;
-  }
-  while(i<2*n/t){
-    a = new HeavyTower({x:i*70+20,y:i+75},state);
-    state.world.push(a);
-    i++;
-  }
-  while(i<3*n/t){
-    a = new SeekingTower({x:i*70+20,y:i+75},state);
-    state.world.push(a);
-    i++;
-  }
-  while(i<4*n/t){
-    a = new MultiShotTower({x:i*70+20,y:i+75},state);
-    state.world.push(a);
-    i++;
-  }
-  
-}
-function makeBuildings(n){
-  i = 0;
-  while(i<n){
-    a = new Building({x:i*70+20,y:i+25},state);
-    state.world.push(a);
-    i++;
-  }
-}
 function makeShips(n,wave){
   i = 0;
   while(i<n){
@@ -51,26 +21,6 @@ function makeShips(n,wave){
     i++;
   }
 }
-
-// a = new GoliathTower({x:1*70+20,y:1+75},state);
-// state.world.push(a);
-// b = new GoliathTower({x:2*70+20,y:2+75},state);
-// state.world.push(b);
-// c = new GoliathTower({x:3*70+20,y:3+75},state);
-// state.world.push(c);
-// d = new GoliathTower({x:4*70+20,y:4+75},state);
-// state.world.push(d);
-
-makeTowers(20, 4);//makeTowers(number of towers, number of types of towers);
-makeBuildings(20);//makeBuildings(number of buildings)
-makeShips(15,1);//makeShips(number of Ships, wave#)
-makeShips(8,2);
-makeShips(5,3);
-makeShips(30,4);
-
-
-
-
 
 
 async function main(state){
