b279941bab3986af50b616e61750d1889c4095f9
Michael Smith <mes258@case.edu>
2018-10-20 23:05:09 -0400

    Fixed Levels

--- main.js
+++ main.js
@@ -13,85 +13,52 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function makeShips(point, r1, r2, n, state){
+function makeShips(point, r1, r2, level, state){
+  n = level*5;
   for(i = 0; i < n; i++){
-    var type = getRandomInt(0,10);
-    if(type < 4){
+    var type = getRandomInt(0,level);
+    if(type <= 4){
       state.world.push(new Ship(makePointIn(point,r1,r2),state));
-    }else if(type < 7){
+    }else if(type <= 7){
       state.world.push(new BigShip(makePointIn(point,r1,r2),state));
-    }else if(type <= 8){
+    }else if(type <= 10){
       state.world.push(new MotherShip(makePointIn(point,r1,r2),state));
-    }else if(type <= 9){
+    }else if(type <= 15){
       state.world.push(new GrandmotherShip(makePointIn(point,r1,r2),state));
     }
   }
-  state.world.push(new BigShip(makePointIn(point,r1,r2),state));
-  
+  return state;
 }
 
 
-function spawnLevel(level, state){
-  //var dist = getFarthestStructure(state);
-  makeShips(zeroVector,800,850,level*5,state);
-}
 
-function getFarthestStructure(state){
-  var farthest = [false, 0];
+var level = 1;
+makeShips(zeroVector,1000,1200,level,state);
+
+function allKilled(state){
   for(i in state.world){
-    if(state.world[i] instanceof Structure){
-      gobj = state.world[i];
-      if(distanceBetween({x:0,y:0}, gobj) > farthest[1]){
-        farthest = [gobj, distanceBetween({x:0,y:0},gobj)];
-      }
+    if(state.world[i] instanceof Ship){
+      return state;
     }
   }
-  return farthest[1];
+  level++;
+  return makeShips(zeroVector,1000,1200,level,state);
 }
 
-function allKilled(level, state){
-  var totalKilled = 0;
-  for(i in state.world){
-    if(state.world[i] instanceof Tower){
-      totalKilled += state.world[i].kills;
-    }
-  }
-  var totalSpawned = 0;
-  var i = 1;
-  while(i <= level){
-    totalSpawned += 5*i;
-    i++;
-  }
-  if(totalSpawned == totalKilled){
-    return true;
-  }else{
-    return false;
-  }
-
-}
-
-//makeShips(zeroVector,800,2850,50,state);
 
 
 async function main(state){
-  var level = 1;
-  spawnLevel(level, state);
   while("Vincent" > "Michael"){
     while(!paused){
       step(state);
       await sleep(25);
 
-
-      if(allKilled(level, state)){
-        level++;
-        spawnLevel(level, state);
-      }      
-
+      state = allKilled(state);
     }
     await sleep(25);
   }
 }
-
+  
 function step(state){
   // state.step();
   state.world.forEach(function(gobject){
