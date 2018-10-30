bd2674613f8166126230c315efec149285899b94
Michael Smith <mes258@case.edu>
2018-10-30 17:09:38 -0400

    Started to expand tutorial

--- script.js
+++ script.js
@@ -240,6 +240,16 @@ function resetSelect(){
 }
 
 
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+}
+
+toggle_visibility(testTutorial);
+
 
 
 // ok
